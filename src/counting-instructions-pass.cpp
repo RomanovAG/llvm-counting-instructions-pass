59a102ad5f5a0ba91cc645de7cd43045e90f92fa
RomanovAG <andpey.romanov@gmail.com>
2024-08-11 22:53:26 +0300

    initial commit

---
+++ src/counting-instructions-pass.cpp
@@ -0,0 +1,90 @@
+#include "llvm/IR/InstIterator.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+
+void
+VisitFunction(Function &Func)
+{
+	DenseMap<unsigned, unsigned> InstCount;
+	
+	for (inst_iterator I = inst_begin(Func), E = inst_end(Func); I != E; I++)
+	{
+		InstCount[I->getOpcode()]++;
+	}
+
+	outs() << "Function " << Func.getName() << ":\n";
+	for (const auto &inst : InstCount)
+	{
+		outs() << "\t" << Instruction::getOpcodeName(inst.first) << ":\t" << inst.second << "\n";
+	}
+}
+
+struct CountingInstructionsPass : PassInfoMixin<CountingInstructionsPass> 
+{
+	PreservedAnalyses 
+	run(Function &Function, FunctionAnalysisManager &AnalysisManager) 
+	{
+		VisitFunction(Function);
+		return (PreservedAnalyses::all());
+	}
+
+	static bool 
+	isRequired(void) 
+	{ 
+		return (true); 
+	}
+};
+} /* namespace */
+
+void
+CallBackForPassBuilder(PassBuilder &PB)
+{
+	PB.registerPipelineParsingCallback(
+		(	[](
+			StringRef Name,
+			FunctionPassManager &FPM,
+			ArrayRef<PassBuilder::PipelineElement>
+			) -> bool
+			{
+				if (Name == "counting-instructions-pass")
+				{
+					FPM.addPass(CountingInstructionsPass());
+					return (true);
+				}
+				else
+				{
+					return (false);
+				}
+			}
+		)
+	);
+} /* CallBackForPassBuider */
+
+PassPluginLibraryInfo 
+getCountingInstructionsPassPluginInfo(void)
+{
+	uint32_t     APIversion =  LLVM_PLUGIN_API_VERSION;
+	const char * PluginName =  "counting-instructions-pass";
+	const char * PluginVersion =  LLVM_VERSION_STRING;
+    
+	PassPluginLibraryInfo info = 
+	{ 
+		APIversion, 
+		PluginName, 
+		PluginVersion, 
+		CallBackForPassBuilder
+	};
+
+	return (info);
+} /* getCountingInstructionsPassPluginInfo */
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() 
+{
+	return (getCountingInstructionsPassPluginInfo());
+} /* llvmGetPassPluginInfo */
